a8e6584a6b1b7c701e7eb4261361a27252fa2336
yy2hi <ypy2.hi@gmail.com>
2024-11-13 18:42:00 +0900

    testcase 추가

--- polls/views.py
+++ polls/views.py
@@ -3,22 +3,43 @@ from django.shortcuts import get_object_or_404, render
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        # 5개 질문 노출, 미래 질문 미노출
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    def get_queryset(self):
+        # 공개 전 질문 미노출
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     # template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
+    # context = {
+    #     "latest_question_list": latest_question_list,
+    # }
     # output = ", ".join([q.question_text for q in latest_question_list])
     # return HttpResponse(context, request)
-    return render(request, "polls/index.html", context)
+    # return render(request, "polls/index.html", context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {'question': question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {'question': question})
     # try:
     #     question = Question.objects.get(pk=question_id)
     # except Question.DoesNotExist:
@@ -29,9 +50,9 @@ def detail(request, question_id):
     # response = "You're looking at question %s."
     # return HttpResponse(response % question_id)
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -45,14 +66,13 @@ def vote(request, question_id):
             {
                 "question": question,
                 "error_message": "You didn't select a choice.",
-            }
+            },
         )
     else:
-        selected_choice_votes = F("votes") + 1
+        selected_choice.votes = F("votes") + 1
         selected_choice.save()
         # 투표 후 항상 HttpResponseRedirect를 반환
         # POST 데이터의 경우. 사용자가 뒤로가기 버튼 누를 시 데이터 두 번 적재 방지
         return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
 
 
-# Create your views here.
