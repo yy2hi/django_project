a0ba69fdab6b28cecb880c31bfbe2f113e52353c
yy2hi <ypy2.hi@gmail.com>
2024-11-13 09:31:16 +0900

    md 작성1

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,58 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import F
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+
+from .models import Question, Choice
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    # output = ", ".join([q.question_text for q in latest_question_list])
+    # return HttpResponse(context, request)
+    return render(request, "polls/index.html", context)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {'question': question})
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, "polls/detail.html", {"question": question})
+
+
+    # response = "You're looking at question %s."
+    # return HttpResponse(response % question_id)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # 재배치 voting form
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            }
+        )
+    else:
+        selected_choice_votes = F("votes") + 1
+        selected_choice.save()
+        # 투표 후 항상 HttpResponseRedirect를 반환
+        # POST 데이터의 경우. 사용자가 뒤로가기 버튼 누를 시 데이터 두 번 적재 방지
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
 
 
 # Create your views here.
